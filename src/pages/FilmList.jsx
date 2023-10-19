2859841120c293a87ea4125e2ab17c55da09d9fb
fredo1975 <fred@autogrif.com>
2023-10-20 00:18:56 +0200

    add pagination

--- src/pages/FilmList.jsx
+++ src/pages/FilmList.jsx
@@ -8,14 +8,24 @@ import { useAxios } from "../helpers/axios-hook";
 import { useEffect, useState } from 'react';
 import Grid from '@mui/material/Unstable_Grid2';
 import Box from '@mui/material/Box';
+import TablePagination from '@mui/material/TablePagination';
 
 const baseURL = '/dvdtheque-service/films';
-
+const paginatedSearch = '/dvdtheque-service/films/paginatedSarch'
 const FilmList = () => {
   const [post, setPost] = useState(null);
-  const { axiosInstance, initialized } = useAxios(null);
+  const {axiosInstance, initialized} = useAxios(null);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [count, setCount] = useState(0);
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
-  const test = 'fredo'
   useEffect(() => {
     //console.log('FilmList initialized',initialized);
     if (!initialized) {
@@ -23,21 +33,28 @@ const FilmList = () => {
     }
     //console.log('axiosInstance',axiosInstance.instance.get(baseURL));
     //const instance = axios.create;
-    //console.log('FilmList axiosInstance',axiosInstance);
-    axiosInstance.instance.get(baseURL, {timeout: 1500})
+    console.log('FilmList rowsPerPage',page);
+    axiosInstance.instance.get(paginatedSearch, {timeout: 1500,
+      params: {
+        query: 'origine:eq:DVD:AND,',
+        sort: '-dateInsertion,+titre',
+        offset: page+1,
+        limit: rowsPerPage
+      }})
     .then((response) => {
-      setPost(response.data);
+      setPost(response.data.content);
+      setCount(response.data.totalElements);
       console.log('response',response);
     }).catch(error => console.error(error));
     
-  }, [initialized]);
+  }, [initialized,page,rowsPerPage]);
 
   if (!post) return null;
 
   return (
 
-<Box sx={{ flexGrow: 1 }}>
-    <Grid container spacing={3}>
+<Box sx={{ flexGrow: 1 }} >
+    <Grid container spacing={{ xs: 1, md: 1 }} columns={{ xs: 2, sm: 4, md: 12 }}>
   {post.map((p, index) => (
     <Grid xs={1} key={index} className="myDiv">
       <Card sx={{ maxWidth: 200 }}>
@@ -63,6 +80,15 @@ const FilmList = () => {
     </Grid>
   ))}
 </Grid>
+
+<TablePagination
+  component="div"
+  count={count}
+  page={page}
+  onPageChange={handleChangePage}
+  rowsPerPage={rowsPerPage}
+  onRowsPerPageChange={handleChangeRowsPerPage}
+/>
 </Box>
   );
 };
